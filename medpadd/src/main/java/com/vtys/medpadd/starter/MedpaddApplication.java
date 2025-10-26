18b65736b3464c25f32f508a6301735ea72225b0
10sinan <msinansonmez10@gmail.com>
2025-10-26 17:36:45 +0300

    starter eklendi

---
+++ medpadd/src/main/java/com/vtys/medpadd/starter/MedpaddApplication.java
@@ -0,0 +1,11 @@
+package com.vtys.medpadd.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedpaddApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedpaddApplication.class, args);
+    }
+}
